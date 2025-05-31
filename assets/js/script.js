982ad2058a74c2b21f6b97c3b07096829c111333
ShakebShamsi <shakebshamsi@hotmail.com>
2025-05-31 22:35:39 +0530

    property section added

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Navbar Toggle Functionality in Mobile View
+ */
+
+const /**{NodeElement} */ $navbar = document.querySelector('[data-navbar]');
+const /**{NodeElement} */ $navToggler = document.querySelector('[data-nav-toggler]');
+
+$navToggler.addEventListener('click', () => $navbar.classList.toggle('active'));
+
+/**   
+ * Header Scroll Functionality
+ */
+
+const /**{NodeElement} */ $header = document.querySelector('[data-header]');
+window.addEventListener('scroll', e => {
+  $header.classList[window.scrollY > 50 ? 'add' : 'remove']('active');   
+})
