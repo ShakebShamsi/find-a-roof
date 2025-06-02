656b996b01aeb3916762edafe970316fd8eba8f8
ShakebShamsi <shakebshamsi@hotmail.com>
2025-06-02 13:19:47 +0530

    WIP: media-querry

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,14 @@ const /**{NodeElement} */ $header = document.querySelector('[data-header]');
 window.addEventListener('scroll', e => {
   $header.classList[window.scrollY > 50 ? 'add' : 'remove']('active');   
 })
+
+
+/* Add to Favourite Toggle Button */
+
+const /** {NodeList} */ $toggleBtns = document.querySelectorAll('[data-toggle-btn]');
+
+$toggleBtns.forEach($toggleBtn => {
+   $toggleBtn.addEventListener('click', () => {
+      $toggleBtn.classList.toggle('active')
+   });
+});
